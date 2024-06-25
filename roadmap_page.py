07b209cc4be5f4fc006891736c2d31e1ae5a5e6a
yinsxie <morenciairenaamelia@gmail.com>
2024-06-25 16:18:33 +0700

    menambahkan page baru

---
+++ roadmap_page.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def show_roadmap_page():
+    st.title("Roadmap to make paper")
---
+++ translate_page.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def show_translate_page():
+    st.title("Translate Paper")
